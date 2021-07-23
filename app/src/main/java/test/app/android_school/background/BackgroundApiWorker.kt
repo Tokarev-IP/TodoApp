3aaf00cf61391a290cf4d749281523d30f474abc
Tokarev-IP <tokarevrab@gmail.com>
2021-07-23 12:56:09 +0300

    work manager 1.0

--- app/src/main/java/test/app/android_school/background/BackgroundApiWorker.kt
+++ app/src/main/java/test/app/android_school/background/BackgroundApiWorker.kt
@@ -6,74 +6,76 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.work.CoroutineWorker
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import test.app.android_school.mvvm.ApiRoomRepository
 //import test.app.android_school.mvvm.ApiRoomRepository
 import test.app.android_school.recycler.TaskData
 import test.app.android_school.retrofit.ApiRepository
 import java.lang.Exception
 import java.util.concurrent.TimeUnit
+import javax.inject.Inject
 
-//class BackgroundApiWorker(private val appContext: Context, workerParams: WorkerParameters):
-//        Worker(appContext, workerParams) {
-//
-////    private val apiRoomRep = ApiRoomRepository(appContext as AppCompatActivity)
-//    private val apiRep = ApiRepository()
-//
-//    override fun doWork(): Result {
-//
-////        for (i in 0..40){
-////            Log.d("FOR", i.toString())
-////            TimeUnit.MILLISECONDS.sleep(250)
-////        }
-//
-//        try {
-//            val list = ApiRoomRepository(appContext as AppCompatActivity).getTasksFromApiRoom()
-//            val count = list.size
-//            Log.d("COUNT", count.toString())
-//
-////            for (i in 0 until count){
-////                when(list[i].actionApi){
-////                    "insert"->{
-////                        apiRep.postTaskApi(
-////                            TaskData(
-////                                list[i].id,
-////                                list[i].text,
-////                                list[i].importance,
-////                                list[i].done,
-////                                list[i].deadline,
-////                                list[i].createdAt,
-////                                list[i].updatedAt
-////                            )
-////                        )
-////                        Log.d("WORKMANAGER", "insert")
-////                    }
-////                    "update"->{
-////                        apiRep.putTaskApi(
-////                            list[i].id,
-////                            TaskData(
-////                                list[i].id,
-////                                list[i].text,
-////                                list[i].importance,
-////                                list[i].done,
-////                                list[i].deadline,
-////                                list[i].createdAt,
-////                                list[i].updatedAt
-////                            )
-////                        )
-////                    }
-////                    "delete"->{
-////                        apiRep.deleteTaskApi(list[i].id)
-////                    }
-////                }
-////                TimeUnit.SECONDS.sleep(5)
-////                ApiRoomRepository(appContext as AppCompatActivity).deleteFromApiRoom(list[i])
-////            }
-//
-//        } catch (e: Exception) {
-//            Log.d("BACKERROR", "exeption")
-//            return Result.failure()
+class BackgroundApiWorker @Inject constructor (appContext: Context,
+                                               workerParams: WorkerParameters,
+                                               private val apiRoomRep: ApiRoomRepository,
+                                               private val apiRep: ApiRepository
+): Worker(appContext, workerParams) {
+
+    override fun doWork(): Result {
+
+//        for (i in 0..40){
+//            Log.d("FOR", i.toString())
+//            TimeUnit.MILLISECONDS.sleep(250)
 //        }
-//
-//        return Result.success()
-//    }
-//
-//}
+
+        try {
+            val list = apiRoomRep.getTasksFromApiRoom()
+            val count = list.size
+            Log.d("COUNT", count.toString())
+
+            for (i in 0 until count){
+                when(list[i].actionApi){
+                    "insert"->{
+                        apiRep.postTaskApi(
+                            TaskData(
+                                list[i].id,
+                                list[i].text,
+                                list[i].importance,
+                                list[i].done,
+                                list[i].deadline,
+                                list[i].createdAt,
+                                list[i].updatedAt
+                            )
+                        )
+                        Log.d("WORKMANAGER", "insert")
+                    }
+                    "update"->{
+                        apiRep.putTaskApi(
+                            list[i].id,
+                            TaskData(
+                                list[i].id,
+                                list[i].text,
+                                list[i].importance,
+                                list[i].done,
+                                list[i].deadline,
+                                list[i].createdAt,
+                                list[i].updatedAt
+                            )
+                        )
+                    }
+                    "delete"->{
+                        apiRep.deleteTaskApi(list[i].id)
+                    }
+                }
+                TimeUnit.SECONDS.sleep(5)
+                apiRoomRep.deleteFromApiRoom(list[i])
+            }
+
+        } catch (e: Exception) {
+            Log.d("BACKERROR", "exeption")
+            return Result.failure()
+        }
+
+        return Result.success()
+    }
+
+}
